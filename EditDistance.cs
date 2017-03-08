ac81a05430536b2ffeeac7b2e4eed46aa4d82cf1
fshs-mit <mithilesh329@hotmail.com>
2017-03-08 02:01:28 -0800

    Update EditDistance.cs

--- EditDistance.cs
+++ EditDistance.cs
@@ -4,15 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EditDistance
-{
-    //Given two words word1 and word2, find the minimum number of steps required to convert word1 to word2. 
-    //(each operation is counted as 1 step.)
-    //You have the following 3 operations permitted on a word:
-    //a) Insert a character
-    //b) Delete a character
-    //c) Replace a character
+//Given two words word1 and word2, find the minimum number of steps required to convert word1 to word2. 
+//(each operation is counted as 1 step.)
+//You have the following 3 operations permitted on a word:
+//a) Insert a character
+//b) Delete a character
+//c) Replace a character
 
+namespace SampleProjects
+{
     public class EditDistanceSolution
     {
         public static void Test()
