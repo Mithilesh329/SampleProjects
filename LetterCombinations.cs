3b99c45d2a008bc8cc2545192309ea8046b36866
fshs-mit <mithilesh329@hotmail.com>
2017-03-13 00:56:41 -0700

    Update LetterCombinations.cs

--- LetterCombinations.cs
+++ LetterCombinations.cs
@@ -6,47 +6,51 @@ using System.Linq;
 //Input:Digit string "23"
 //Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
 
-public class SampleProjects {
-    public IList<string> LetterCombinations(string digits) 
+public namespace SampleProjects
+{
+    public class LetterCombinationsSolution 
     {
-        if(string.IsNullOrWhiteSpace(digits))
+        public IList<string> LetterCombinations(string digits) 
         {
-            return new List<string>();
-        }
-        
-        Dictionary<char,string>  map = new Dictionary<char, string>();
-        map.Add('2', "abc");
-        map.Add('3', "def");
-        map.Add('4', "ghi");
-        map.Add('5', "jkl");
-        map.Add('6', "mno");
-        map.Add('7', "pqrs");
-        map.Add('8', "tuv");
-        map.Add('9', "wxyz");
-        
-        Queue<string> queue = new Queue<string>();
-        queue.Enqueue("");
-        
-        foreach(char c in digits)
-        {
-            if(!map.ContainsKey(c))
+            if(string.IsNullOrWhiteSpace(digits))
             {
-                continue;
+                return new List<string>();
             }
-            
-            string chars = map[c];
-            int count = queue.Count;
-            for(int i=0;i<count; i++)
+
+            Dictionary<char,string>  map = new Dictionary<char, string>();
+            map.Add('2', "abc");
+            map.Add('3', "def");
+            map.Add('4', "ghi");
+            map.Add('5', "jkl");
+            map.Add('6', "mno");
+            map.Add('7', "pqrs");
+            map.Add('8', "tuv");
+            map.Add('9', "wxyz");
+
+            Queue<string> queue = new Queue<string>();
+            queue.Enqueue("");
+
+            foreach(char c in digits)
             {
-                string temp = queue.Dequeue();
-                foreach(char ch in chars)
+                if(!map.ContainsKey(c))
                 {
-                    queue.Enqueue(temp+ch);
+                    continue;
+                }
+
+                string chars = map[c];
+                int count = queue.Count;
+                for(int i=0;i<count; i++)
+                {
+                    string temp = queue.Dequeue();
+                    foreach(char ch in chars)
+                    {
+                        queue.Enqueue(temp+ch);
+                    }
                 }
             }
+
+            return queue.ToList();
+
         }
-        
-        return queue.ToList();
-        
     }
 }
