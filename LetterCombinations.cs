a3809b24a40efd05a40fe6316f11e40c1a9c65a4
fshs-mit <mithilesh329@hotmail.com>
2017-03-13 00:45:45 -0700

    LetterCombinations.cs
    LetterCombinations file

---
+++ LetterCombinations.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+//Given a digit string, return all possible letter combinations that the number could represent.
+//Input:Digit string "23"
+//Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+
+public class SampleProjects {
+    public IList<string> LetterCombinations(string digits) 
+    {
+        if(string.IsNullOrWhiteSpace(digits))
+        {
+            return new List<string>();
+        }
+        
+        Dictionary<char,string>  map = new Dictionary<char, string>();
+        map.Add('2', "abc");
+        map.Add('3', "def");
+        map.Add('4', "ghi");
+        map.Add('5', "jkl");
+        map.Add('6', "mno");
+        map.Add('7', "pqrs");
+        map.Add('8', "tuv");
+        map.Add('9', "wxyz");
+        
+        Queue<string> queue = new Queue<string>();
+        queue.Enqueue("");
+        
+        foreach(char c in digits)
+        {
+            if(!map.ContainsKey(c))
+            {
+                continue;
+            }
+            
+            string chars = map[c];
+            int count = queue.Count;
+            for(int i=0;i<count; i++)
+            {
+                string temp = queue.Dequeue();
+                foreach(char ch in chars)
+                {
+                    queue.Enqueue(temp+ch);
+                }
+            }
+        }
+        
+        return queue.ToList();
+        
+    }
+}
